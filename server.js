99469b7db290b593ff9b9d42bb4f8f30f7d07459
Hamdallah Sulaimon <hamdysj@gmail.com>
2022-03-04 13:31:22 +0100

    UPDATE

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+//ENVIRONMENT VARIABLE
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 3000;
+//Set by express: Shows the environment the app is runnin
+//console.log(app.get('env'));
+//Set by node: Shows the environment the app is runnin
+//console.log(process.env);
+
+
+//console.log(process.env);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}......`)
 })
